b2286b5b3523a121d41444db27e3225c57c45cda
EL HOUCINE AOUASSAR <me@awixor.com>
2026-01-22 14:23:45 +0100

    Fix theme issue

--- providers/providers.tsx
+++ providers/providers.tsx
@@ -4,13 +4,23 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { WagmiProvider } from "wagmi";
 import { config } from "@/config/config";
 import { useState } from "react";
+import { ThemeProvider } from "./theme-provider";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(() => new QueryClient());
 
   return (
     <QueryClientProvider client={queryClient}>
-      <WagmiProvider config={config}>{children}</WagmiProvider>
+      <WagmiProvider config={config}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </WagmiProvider>
     </QueryClientProvider>
   );
 }
