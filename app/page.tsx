c8b4b913826009b7cd6bd0a878497f7fbf3dea69
EL HOUCINE AOUASSAR <me@awixor.com>
2026-01-23 15:07:08 +0100

    Update app logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import { TipJarCard } from "@/components/tip/tip-card";
 import { ConnectWallet } from "@/components/wallet/connect-wallet";
 import { AdminPanel } from "@/components/admin/admin-panel";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-start gap-8 bg-zinc-50 px-4 py-16 dark:bg-zinc-950 sm:py-32">
-      <div className="w-full flex flex-col items-center gap-4 max-w-4xl">
+      <Image src="/ms-krabs.png" alt="MS Krabs" width={120} height={120} />
+      <div className="w-full flex flex-col items-center gap-4 max-w-4xl ">
         <ConnectWallet />
         <div className="flex gap-2 w-full sm:flex-row flex-col justify-center">
           <AdminPanel />
